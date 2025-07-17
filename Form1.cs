2c82a0b5e67ba9091159c837610ac9f298c29d3a
Shivam Kumar <115875580+Badshahshvm@users.noreply.github.com>
2025-07-17 19:02:10 +0530

    Create Form1.cs

---
+++ Form1.cs
@@ -0,0 +1,58 @@
+namespace WinFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Patna");
+            comboBox1.Items.Add("Delhi");
+            comboBox1.Items.Add("Mumbai");
+            comboBox1.Items.Add("Bangalore");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
